e55b620324ce8d9a55e63315408cc4159030e060
Chris Sinco <csinco.design@gmail.com>
2021-02-28 21:31:39 -0800

    Add theme file

---
+++ src/main/kotlin/intellijicons/theme.kt
@@ -0,0 +1,21 @@
+package intellijicons
+
+import androidx.compose.material.Colors
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.ui.graphics.Color
+
+val lightBackground = Color(0xfff9f9f9)
+val darkBackground = Color(0xff3c3f41)
+
+fun lightThemeColors(): Colors {
+    return lightColors().copy(
+
+    )
+}
+
+fun darkThemeColors(): Colors {
+    return darkColors().copy(
+
+    )
+}
